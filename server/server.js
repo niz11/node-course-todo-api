6569b4cbe29439e518c03d5aba4a2c1c51af91ef
nizan goldstein <nizan goldstein>
2018-04-08 22:24:03 +0200

    Get todos

--- server/server.js
+++ server/server.js
@@ -15,18 +15,28 @@ app.use(bodyParser.json()); // can Send json to our app now
 
 // Creat new todo
 app.post('/todos', (req,res)=>{
-var todo = new Todo({
-  text: req.body.text
-})
-todo.save().then((doc)=>{
-  res.send(doc);
-}, (e) =>{
-  res.status(400).send(e);
-});
+  var todo = new Todo({
+    text: req.body.text
+  })
+  todo.save().then((doc)=>{
+    res.send(doc);
+  }, (e) =>{
+    res.status(400).send(e);
+  });
 
 // // If I send a post usl from user, I'll get in the termial what json file I wrote and sent in postman
 //   console.log(req.body);
 });
+
+app.get('/todos', (req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({
+      todos // It's got not sending todos as an array! we send it as an object, and in the future we can work with it.
+    });
+  }, (e)=>{
+    res.status(400).send(e);
+  })
+})
 
 app.listen(3000 , ()=>{
   console.log('Started on port 3000');
