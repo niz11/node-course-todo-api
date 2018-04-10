cda29c9ca3a4b48e891468f616c849505b04a5df
nizan goldstein <nizan goldstein>
2018-04-10 14:55:38 +0200

    Adding config to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 // Confuguring the database
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONDODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports ={
   mongoose
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ var {User} = require('./models/users');
 const {ObjectID} = require('mongodb');
 
 var app = express();
+const port = process.env.PORT || 3000 ;
 
 app.use(bodyParser.json()); // can Send json to our app now
 // Crud - creat ,read , update and delete
@@ -56,8 +57,8 @@ app.get('/todos/:id', (req,res)=>{
   })
 })
 
-app.listen(3000 , ()=>{
-  console.log('Started on port 3000');
+app.listen(port , ()=>{
+  console.log(`Started on port ${port}`);
 });
 
 module.exports ={app};
