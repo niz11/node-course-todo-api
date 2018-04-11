3cc6144885e8c794a0f154465147231bcdb27f0a
nizan goldstein <nizan goldstein>
2018-04-11 16:48:39 +0200

    Another try to fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 // Confuguring the database
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONDODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports ={
   mongoose
